a8199db892dd5d26809772e7c54c8ae12e534573
Dairon Ian Garcia Roque <daironigr@gmail.com>
2023-10-02 13:36:32 +0200

    add comming soon

--- console/controllers/TestController.php
+++ console/controllers/TestController.php
@@ -6,17 +6,17 @@ use common\models\Business;
 use common\models\User;
 use Da\User\Factory\TokenFactory;
 use Yii;
+use yii\symfonymailer\Message;
 
 class TestController extends BaseController
 {
-    public function actionTest(){
-        $stripe = new \Stripe\StripeClient(Yii::$app->params['stripe.secretKey']);
-        $prices = $stripe->prices->search([
-            'query' => "product: 'prod_OgqjpThAnSxBaL' AND active: 'true'"
-        ]);
-
-        foreach ($prices->data as $price) {
-            print_r($price->active);
-        }
+    public function actionTest()
+    {
+        Yii::$app->mailer->send((new Message())
+            ->setSubject("Prueba")
+            ->setFrom(Yii::$app->params['senderEmail'])
+            ->setTo('daironigr@gmail.com')
+            ->setTextBody("Probando email")
+        );
     }
 }
