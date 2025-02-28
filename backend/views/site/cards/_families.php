2e4d08f911d06c86e1f27d48426fb2b853cb7f53
Daniel <dgonzalez9723@gmail.com>
2025-02-28 07:41:33 -0500

    fix scroll families en dash

--- backend/views/site/cards/_families.php
+++ backend/views/site/cards/_families.php
@@ -8,7 +8,6 @@ $families = $business->getRecipeCategories()->andWhere(['type' => \common\models
 usort($families, function($a, $b){
     return $b->getRecipes()->count() - $a->getRecipes()->count();
 });
-$families = array_slice($families, 0, 3, false);
 ?>
 <div class="p-2">
     <div class="card bg-secondary text-white mb-3">
@@ -16,16 +15,18 @@ $families = array_slice($families, 0, 3, false);
             <span class="card-title"><strong>Familias</strong></span>
         </div>
         <div class="card-body">
-            <table class="table text-white table-borderless">
-                <tbody>
-                <?php foreach ($families as $family): ?>
-                    <tr>
-                        <td><?= $family['name'] ?></td>
-                        <td><?= $family->getRecipes()->count() ?></td>
-                    </tr>
-                <?php endforeach; ?>
-                </tbody>
-            </table>
+            <div class="table-responsive" >
+                <table class="table text-white table-borderless">
+                    <tbody>
+                    <?php foreach ($families as $family): ?>
+                        <tr>
+                            <td><?= $family['name'] ?></td>
+                            <td><?= $family->getRecipes()->count() ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
