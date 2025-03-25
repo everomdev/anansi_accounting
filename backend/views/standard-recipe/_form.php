a21f13b388fae0437e40a773cd972c3c63150609
Daniel <dgonzalez9723@gmail.com>
2025-03-25 13:44:57 -0400

    fix name

--- backend/views/standard-recipe/_form.php
+++ backend/views/standard-recipe/_form.php
@@ -60,8 +60,16 @@ $currencySymbol = preg_replace('/[a-zA-Z]/', '', $currencySymbol);
                     <?php if (!$model->isNewRecord): ?>
                         <?= $form->field($model, 'title', [
                             'template' => "<div class='row mb-3'>{label}<div class='col-sm-8'>{input}</div></div>"
-                        ])->textInput(['id' => 'title-input', 'value' => $model->title])->label(
-                            $model->type == \common\models\StandardRecipe::STANDARD_RECIPE_TYPE_MAIN ? 'Nombre de la receta' : 'Nombre de la subreceta', 
+                        ])->textInput([
+                            'id' => 'title-input', 
+                            'value' => $model->title, 
+                            'placeholder' => $model->type == \common\models\StandardRecipe::STANDARD_RECIPE_TYPE_MAIN 
+                                ? Yii::t('app', 'Ingrese el nombre de la receta') 
+                                : Yii::t('app', 'Ingrese el nombre de la subreceta')
+                        ])->label(
+                            $model->type == \common\models\StandardRecipe::STANDARD_RECIPE_TYPE_MAIN 
+                                ? 'Nombre de la receta' 
+                                : 'Nombre de la subreceta', 
                             ['class' => 'col-sm-4 text-start']
                         ) ?>
                     <?php endif; ?>
