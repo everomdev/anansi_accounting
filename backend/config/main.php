0060cc3ef6c039836f6da9cf9c64f92761ebb566
Daniel <dgonzalez9723@gmail.com>
2025-04-29 11:58:50 -0400

    fix

--- backend/config/main.php
+++ backend/config/main.php
@@ -85,7 +85,8 @@ return [
             'targets' => [
                 [
                     'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
+                    'levels' => ['error', 'warning','trace'],
+                    'logFile' => '@runtime/logs/app.log',
                 ],
             ],
         ],
