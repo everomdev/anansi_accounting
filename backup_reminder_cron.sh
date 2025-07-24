1104b413f58a4287c88d299b39b001c0b246dce1
Daniel <dgonzalez9723@gmail.com>
2025-07-24 11:22:23 -0400

    fix cron

--- backup_reminder_cron.sh
+++ backup_reminder_cron.sh
@@ -8,7 +8,8 @@ cd /opt/docker/projects/anansi_accounting/
 
 
 # Ejecutar el comando que activa los recordatorios
-php yii backup/activate-reminder
+#php yii backup/activate-reminder
+docker compose -f docker-compose.prod.yml exec php yii backup/activate-reminder
 
 # Log opcional
 echo "$(date): Backup reminder cron ejecutado" >> logs/backup_reminder.log
