900510cb4250486b290309c146d1dd6b9c302460
Daniel <dgonzalez9723@gmail.com>
2025-10-22 16:03:52 -0400

    fix

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,5 +1,17 @@
 <?php
 /* @var $this yii\web\View */
+
+$this->title = Yii::$app->name;
+?>
+
+<div class="alert alert-info" role="alert">
+    <h4 class="alert-heading">¡Trabajando en mejoras del Dashboard!</h4>
+    <p>Estamos trabajando en optimizar y mejorar el dashboard para ofrecerte una mejor experiencia. Pronto estará disponible con nuevas funcionalidades.</p>
+    <hr>
+    <p class="mb-0">Gracias por tu paciencia.</p>
+</div>
+
+<?php
 /*
 $this->title = Yii::$app->name;
 $business = \backend\helpers\RedisKeys::getBusiness();
