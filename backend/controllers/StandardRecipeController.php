21373bdd8f7311e7285446ba86a8cc8f15b17c72
Daniel <dgonzalez9723@gmail.com>
2025-11-26 10:42:51 -0500

    degug

--- backend/controllers/StandardRecipeController.php
+++ backend/controllers/StandardRecipeController.php
@@ -3116,6 +3116,9 @@ public function actionAnalytics($family = 'all', $sort = null, $direction = 'asc
      
      $insumosRow = 2;
      foreach ($ingredientStock as $ingredient) {
+        if ($ingredient->ingredient === 'JITOMATE') {
+            die(var_dump($ingredient->lastPrice));
+        }
          $insumosSheet->setCellValue('A'.$insumosRow, $ingredient->ingredient);
          $insumosSheet->setCellValue('B'.$insumosRow, $ingredient->quantity);
          $insumosSheet->setCellValue('C'.$insumosRow, $ingredient->portion_um);
