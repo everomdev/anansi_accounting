f0f4624413d3b211497b1e0e8cb277a6c4211287
Daniel <dgonzalez9723@gmail.com>
2025-12-06 11:45:18 -0500

    fix

--- backend/views/expense/create.php
+++ backend/views/expense/create.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Expense */
-
+$this->title = Yii::t('app', 'Crear Gasto');
 $this->params['breadcrumbs'][] = ['label' => 'Gastos', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
