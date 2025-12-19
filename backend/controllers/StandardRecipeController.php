90d0f4c02788895235335507d6f06a980cc63f53
Daniel <dgonzalez9723@gmail.com>
2025-12-19 10:09:33 -0500

    fix

--- backend/controllers/StandardRecipeController.php
+++ backend/controllers/StandardRecipeController.php
@@ -2915,7 +2915,7 @@ public function actionAnalytics($family = 'all', $sort = null, $direction = 'asc
                 $ingredientsSheet->setCellValue('B'.$ingredientsRow, strtolower($ingredientRelation->ingredient->ingredient));
                 $ingredientsSheet->setCellValue('C'.$ingredientsRow, $ingredientRelation->quantity);
                 $ingredientsSheet->setCellValue('D'.$ingredientsRow, $ingredientRelation->ingredient->portion_um);
-                $ingredientsSheet->setCellValue('E'.$ingredientsRow, $ingredientRelation->lastPrice);
+                $ingredientsSheet->setCellValue('E'.$ingredientsRow, $ingredientRelation->lastUnitPrice);
                 $ingredientsRow++;
             }
            //die(var_dump($recipe->getSubStandardRecipes()->all()));
