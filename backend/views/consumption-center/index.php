cd2e1f60b3eaca0dbb9079a8e4019f57dc3381dd
Daniel <dgonzalez9723@gmail.com>
2026-02-20 11:10:42 -0500

    fix

--- backend/views/consumption-center/index.php
+++ backend/views/consumption-center/index.php
@@ -32,7 +32,21 @@ $this->params['breadcrumbs'][] = $this->title;
 
             [
                     'class' => 'yii\grid\ActionColumn',
-                'template' => "{update} {delete}"
+                'template' => "{update} {delete}",
+                'buttons' => [
+                    'delete' => function ($url, $model, $key) {
+                        return Html::a(
+                            '<svg aria-hidden="true" style="display:inline-block;font-size:inherit;height:1em;overflow:visible;vertical-align:-.125em;width:.875em" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M32 464a48 48 0 0048 48h288a48 48 0 0048-48V128H32zm272-256a16 16 0 0132 0v224a16 16 0 01-32 0zm-96 0a16 16 0 0132 0v224a16 16 0 01-32 0zm-96 0a16 16 0 0132 0v224a16 16 0 01-32 0zM432 32H312l-9-19a24 24 0 00-22-13H167a24 24 0 00-22 13l-9 19H16A16 16 0 000 48v32a16 16 0 0016 16h416a16 16 0 0016-16V48a16 16 0 00-16-16z"></path></svg>',
+                            '#',
+                            [
+                                'title' => Yii::t('yii', 'Delete'),
+                                'aria-label' => Yii::t('yii', 'Delete'),
+                                'class' => 'text-warning delete-cc-link',
+                                'data-url' => $url,
+                            ]
+                        );
+                    }
+                ]
             ],
         ],
     ]); ?>
@@ -40,3 +54,40 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php Pjax::end(); ?>
 
 </div>
+
+<?php
+$this->registerJs(<<<JS
+$(document).on('click', '.delete-cc-link', function(e) {
+    e.preventDefault();
+    
+    var deleteUrl = $(this).data('url');
+    
+    // Primera confirmación simple
+    if (confirm('¿Está seguro de que desea eliminar este Centro de Consumo?')) {
+        // Segunda confirmación con advertencia detallada
+        if (confirm('⚠️ ADVERTENCIA: Este Centro de Consumo puede estar asociado a movimientos, inventarios y usuarios. Si lo elimina, afectará el historial y los datos relacionados. Esta acción no se puede deshacer.\\n\\n¿Realmente desea continuar con la eliminación?')) {
+            // Crear un formulario y enviarlo
+            var form = $('<form>', {
+                'method': 'POST',
+                'action': deleteUrl
+            });
+            
+            var csrfParam = $('meta[name="csrf-param"]').attr('content');
+            var csrfToken = $('meta[name="csrf-token"]').attr('content');
+            
+            form.append($('<input>', {
+                'type': 'hidden',
+                'name': csrfParam,
+                'value': csrfToken
+            }));
+            
+            $('body').append(form);
+            form.submit();
+        }
+    }
+    
+    return false;
+});
+JS
+, \yii\web\View::POS_READY);
+?>
