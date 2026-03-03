b13a0af0957a3896ed0e1be715f989ef6ae52068
Daniel <dgonzalez9723@gmail.com>
2026-03-03 09:50:07 -0500

    fix

--- backend/views/user/admin/update_user.php
+++ backend/views/user/admin/update_user.php
@@ -178,7 +178,7 @@ $this->registerJsVar('searchPlaceholder', Yii::t('app', "Search"));
         <div class="row">
             <div class="col-md-12">
                 <label>Permisos adicionales <small class="text-muted">(opcional)</small></label>
-                <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#permissionsModal" data-additional='<?= json_encode($model->_permissions ?: []) ?>'>Seleccionar permisos adicionales</button>
+                <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#permissionsModal" data-additional='<?= json_encode($model->_permissions ?: []) ?>'>Gestionar permisos</button>
                 <input type="hidden" name="CreateUserForm[_permissions]" id="permissions-hidden">
             </div>
         </div>
@@ -229,7 +229,7 @@ $this->registerJsVar('searchPlaceholder', Yii::t('app', "Search"));
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="permissionsModalLabel">Seleccionar permisos adicionales</h5>
+                <h5 class="modal-title" id="permissionsModalLabel">Gestionar permisos</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
