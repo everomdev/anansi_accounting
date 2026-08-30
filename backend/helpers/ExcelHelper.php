e2a0b2ccefd72dcc9e094ddcfa38874f1e72f36a
Daniel <dgonzalez9723@gmail.com>
2026-08-30 19:14:36 +0200

    fix

--- backend/helpers/ExcelHelper.php
+++ backend/helpers/ExcelHelper.php
@@ -2208,7 +2208,7 @@ if ($ccRow > 2) {
             $activeWorksheet->setCellValue("G$currentRow", $ingredient->portion_um);
             $activeWorksheet->setCellValue("H$currentRow", $ingredient->yield);
             $activeWorksheet->setCellValue("I$currentRow", $ingredient->portions_per_unit);
-            $activeWorksheet->setCellValue("J$currentRow", $ingredient->lastUnitPrice);
+            $activeWorksheet->setCellValue("J$currentRow", $ingredient->lastPrice);
             $activeWorksheet->setCellValue("K$currentRow", $ingredient->observations);
 
             $currentRow++;
